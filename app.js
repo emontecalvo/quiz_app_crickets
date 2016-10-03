01f09c0b63c7c1a5a2def36cef137d0f8433fdec
emontecalvo <emilymontecalvo@gmail.com>
2016-10-03 13:31:08 -0700

    barebones working quiz

--- app.js
+++ app.js
@@ -37,6 +37,8 @@ var state = {
       image: "IMAGE5",
     } ],
     current_question: 0,
+    score: 0,
+    attempts: 0,
 };
 
 
@@ -49,19 +51,49 @@ $(".start_button").click(function(){
 
 function showQuestion(){
   $(".answer_options").show();
-    console.log(state.exoticFood[state.current_question]["question"]);
     var q = state.exoticFood[state.current_question]["question"];
     $(".title").html(q);
-    $("li:nth-child(1)").html("<li><input type='checkbox'/>" + state.exoticFood[state.current_question]["answers"][0]) + "</li>";
-    $("li:nth-child(2)").html("<li><input type='checkbox'/>" + state.exoticFood[state.current_question]["answers"][1]) + "</li>";
-    $("li:nth-child(3)").html("<li><input type='checkbox'/>" + state.exoticFood[state.current_question]["answers"][2]) + "</li>";
-    $("li:nth-child(4)").html("<li><input type='checkbox'/>" + state.exoticFood[state.current_question]["answers"][3]) + "</li>";
+
+
+
+    $("#first_choice").text(state.exoticFood[state.current_question]["answers"][0]);
+    $("#second_choice").text(state.exoticFood[state.current_question]["answers"][1]);
+    $("#third_choice").text(state.exoticFood[state.current_question]["answers"][2]);
+    $("#fourth_choice").text(state.exoticFood[state.current_question]["answers"][3]);
 };
-// $("#element2").replaceWith("This is the second element");
+
+  $(".choice_button").click(function() {
+    var user_guess = $(this).text();
+    checkAnswer(user_guess);
+  });
+
+function checkAnswer(user_guess){
+  if (user_guess === state.exoticFood[state.current_question]["correctAnswer"]) {
+    state.score += 1;
+    state.attempts += 1;
+    nextQuestion();
+  } else {
+    state.attempts += 1;
+  }
+};
 
 function nextQuestion(){
   state.current_question += 1;
-  showQuestion();
+  console.log(state.current_question, state.exoticFood.length, "--position");
+  if (state.current_question < state.exoticFood.length) {
+    showQuestion();
+  } else {
+    $(".title").text("Your final score is: " + state.score + " out of " + state.attempts);
+    rePlay();
+  }
+};
+
+function rePlay() {
+    $(".start_button").show();
+    state.current_question = 0;
+    state.score = 0;
+    state.attempts = 0;
+    $(".answer_options").hide();
 };
 
 //click on an option, if the answer is correct 
